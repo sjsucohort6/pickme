ce5f1ab6e9d1cb8ed4eae28b1e22e5c2276f1946
cpunekar <chetan.punekar@sjsu.edu>
2016-08-13 12:35:40 -0700

    Updates

---
+++ src/main/java/edu/sjsu/cmpe202/cli/Notifier.java
@@ -0,0 +1,30 @@
+package edu.sjsu.cmpe202.cli;
+
+import edu.sjsu.cmpe202.db.dao.NotificationDao;
+import edu.sjsu.cmpe202.db.domain.Notification;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author rwatsh on 8/6/16.
+ */
+@Data
+public class Notifier {
+    private String notify_id;
+    private String notifyuser_id;
+    private String date;
+    private String message;
+
+    public static void printNotificationMenu(){
+        System.out.println("\t [1] Send Notifier");
+        System.out.println("\t [2] Go back to main menu");
+    }
+
+    public List<String> showNotification() {
+         List<Notification> result = NotificationDao.getNotifications();
+        System.out.println("\t\t Show notifications to the user " + result);
+        return null;
+    }
+
+}
